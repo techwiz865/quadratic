e5fefc379a970faaa7814dd05fbece3ecbec3d3a
Sanihith K. <147875298+techwiz865@users.noreply.github.com>
2024-04-09 15:32:23 +0000

    hy

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 function calculate() {
-    a = document.getElementById('a').value;
-    b = document.getElementById('b').value;
-    c = document.getElementById('c').value;
-    output1 = actualCalc1(a, b, c);
-    output2 = actualCalc2(a, b, c);
-    document.getElementById('output').innerHTML = output1 + " and " + output2;
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let c = document.getElementById('c').value;
+
+    if ((b * b - 4 * a * c) < 0) {
+        document.getElementById('output').innerHTML = "No real roots exist";
+    } else {
+        let output1 = actualCalc1(a, b, c);
+        let output2 = actualCalc2(a, b, c);
+        check(output1, output2);
+    }
 }
 
-function actualCalc(a, b, c) {
+function check(output1, output2) {
+    if (output1 == output2) {
+        document.getElementById('output').innerHTML = "Only one root: " + output1;
+    } else {
+        document.getElementById('output').innerHTML = "Root 1: " + output1 + " Root 2: " + output2;
+    }
+}
+
+function actualCalc1(a, b, c) {
     return (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
 }
-
 
 function actualCalc2(a, b, c) {
     return (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
