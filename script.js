199e40fdbe344fd8ddb2344e9ba5e6604f0008f2
Sanihith K. <147875298+techwiz865@users.noreply.github.com>
2024-04-09 15:21:47 +0000

    added script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function calculate() {
+    a = document.getElementById('a').value;
+    b = document.getElementById('b').value;
+    c = document.getElementById('c').value;
+    output1 = actualCalc1(a, b, c);
+    output2 = actualCalc2(a, b, c);
+    document.getElementById('output').innerHTML = output1 + " and " + output2;
+}
 
+function actualCalc(a, b, c) {
+    return (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+}
+
+
+function actualCalc2(a, b, c) {
+    return (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+}
